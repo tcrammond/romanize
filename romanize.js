f706cc9782d391d8ac198d594917ce044841638b
Tyler Crammond <tylerchc92@gmail.com>
2015-09-20 17:20:05 +0100

    Split construction of numeral into thousands, hundreds, tens and ones. Add check for repeating and amend output. Doesn't work for high numbers, refactoring

--- romanize.js
+++ romanize.js
@@ -3,6 +3,7 @@
  */
 
 function romanize(number) {
+    console.log(number + ": ");
 
     var numerals = [
         {
@@ -26,6 +27,10 @@ function romanize(number) {
             number: 100
         },
         {
+            letter: 'D',
+            number: 500
+        },
+        {
             letter: 'M',
             number: 1000
         }
@@ -40,18 +45,75 @@ function romanize(number) {
         }
     }
 
-    var result = '';
-
-    function run() {
-        var highest = getHighestNumeral(number);
-        result += highest.letter;
-        number -= highest.number;
-
-        if (number > 0) run();
+    function getNextHighestNumeral(numeral) {
+        return numerals[numerals.indexOf(numeral) + 1];
     }
 
-    run();
+    function getNextLowestNumeral(numeral) {
+        return numerals[Math.max(0, numerals.indexOf(numeral) - 1)];
+    }
 
+    var result = '';
+
+    var ones = 0;
+    var tens = 0;
+    var hundreds = 0;
+    var thousands = 0;
+
+    if (number >= 1000) {
+        thousands = Math.floor(number / 1000) * 1000;
+        number -= thousands;
+    }
+    if (number >= 100) {
+        hundreds = Math.floor(number / 100) * 100;
+        number -= hundreds;
+    }
+    if (number >= 10) {
+        tens = Math.floor(number / 10) * 10;
+        number -= tens;
+    }
+    if (number > 0) ones = number;
+
+    //console.log("split - ", thousands, hundreds, tens, ones);
+
+    function run(num) {
+        var highest = getHighestNumeral(num);
+        var nextHighest;
+
+        if (num <= 0) return;
+
+        /*
+        If it's about to repeat / end, amend
+         */
+        if ((num / highest.number) === 4) {
+            nextHighest = getNextHighestNumeral(getNextHighestNumeral(highest));
+            result = result.slice(0, result.length - 1);
+            result += highest.letter;
+            result += nextHighest.letter;
+            num = 0;
+
+            return;
+        }
+
+        /*
+        If the next iteration would start repeating, we need to change our course
+         */
+
+
+
+        result += highest.letter;
+        num -= highest.number;
+
+
+        if (num > 0) run(num);
+    }
+
+    run(thousands);
+    run(hundreds);
+    run(tens);
+    run(ones);
+
+    console.log(result);
     return result;
 }
 
@@ -62,3 +124,9 @@ romanize(19);
 romanize(90);
 romanize(99);
 romanize(100);
+romanize(123);
+romanize(139);
+romanize(150);
+romanize(199);
+romanize(499);
+romanize(501);
